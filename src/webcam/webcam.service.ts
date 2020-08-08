0c2e9846b683269daa6a6bf661fbae90f63eea42
Brandon Piña <brandonpi265@gmail.com>
2020-08-08 17:42:54 -0500

    Serve client at the same time as server

--- src/webcam/webcam.service.ts
+++ src/webcam/webcam.service.ts
@@ -1,35 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import * as cv from 'opencv4nodejs'
+import * as cv from 'opencv4nodejs';
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'http';
 
 @Injectable()
 @WebSocketGateway()
 export class WebcamService {
+  @WebSocketServer()
+  server: Server;
 
-    
-    
+  getCam() {}
 
-    @WebSocketServer()
-    server :Server;
+  onModuleInit() {
+    let cvCap = new cv.VideoCapture(0);
+    //cvCap.set(cv.CAP_PROP_FRAME_WIDTH,300);
+    // cvCap.set(cv.CAP_PROP_FRAME_HEIGHT,300);
+    let interval = 1000 / cvCap.get(cv.CAP_PROP_FPS);
 
-    getCam()
-    {
-    
-    }
+    setInterval(() => {
+      //const frame = cvCap.read();
+      const frameAsync = cvCap.readAsync().then(frame => {
+        const image = cv.imencode('.jpg', frame).toString('base64');
+        //console.log(image);
+        this.server.emit('frame', image);
+       
+        console.count("Frame\t");
+      });
 
-    onModuleInit(){
-        
-        let cvCap = new cv.VideoCapture(0);
-        //cvCap.set(cv.CAP_PROP_FRAME_WIDTH,300);
-       // cvCap.set(cv.CAP_PROP_FRAME_HEIGHT,300);
-        let interval = 1000/30
-        
-        setInterval(()=>{
-            const frame = cvCap.read();
-            const image = cv.imencode(".jpg",frame).toString('base64')
-            //console.log(image);
-            this.server.emit('frame',image);
-        },interval);
-    }
+      
+      console.count("Interval\t");
+    }, interval);
+  }
 }
